169a0819b24fc9e79a2f58fda020e598b08423d6
Maxime-Maguet <maxime.maguet@gmail.com>
2026-09-03 15:39:05 +0200

    feat(server): migrate to Express and add project config

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/about", (req, res) =>
+  res.sendFile(path.join(__dirname, "about.html")),
+);
+app.get("/contact-me", (req, res) =>
+  res.sendFile(path.join(__dirname, "contact-me.html")),
+);
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+  // This is important!
+  // Without this, any startup errors will silently fail
+  // instead of giving you a helpful error message.
+  if (error) {
+    throw error;
+  }
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
